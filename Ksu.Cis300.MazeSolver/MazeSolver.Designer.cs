953da36161ba793b9e7c6daf672ba37d8b4b085c
unknown <dannylovell11@gmail.com>
2016-09-03 22:39:19 -0500

    First commit

---
+++ Ksu.Cis300.MazeSolver/MazeSolver.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Ksu.Cis300.MazeSolver
+{
+    partial class MazeSolver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxMaze = new Ksu.Cis300.MazeLibrary.Maze();
+            this.uxButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uxMaze
+            // 
+            this.uxMaze.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxMaze.AutoSize = true;
+            this.uxMaze.Location = new System.Drawing.Point(13, 13);
+            this.uxMaze.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.uxMaze.Name = "uxMaze";
+            this.uxMaze.PathColor = System.Drawing.SystemColors.Highlight;
+            this.uxMaze.Size = new System.Drawing.Size(495, 399);
+            this.uxMaze.TabIndex = 0;
+            this.uxMaze.MouseClick += new System.Windows.Forms.MouseEventHandler(this.uxMaze_MouseClick);
+            // 
+            // uxButton
+            // 
+            this.uxButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.uxButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxButton.Location = new System.Drawing.Point(185, 434);
+            this.uxButton.Name = "uxButton";
+            this.uxButton.Size = new System.Drawing.Size(157, 42);
+            this.uxButton.TabIndex = 1;
+            this.uxButton.Text = "New Maze";
+            this.uxButton.UseVisualStyleBackColor = true;
+            this.uxButton.Click += new System.EventHandler(this.uxButton_Click);
+            // 
+            // MazeSolver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 488);
+            this.Controls.Add(this.uxButton);
+            this.Controls.Add(this.uxMaze);
+            this.Name = "MazeSolver";
+            this.Text = "Maze Solver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MazeLibrary.Maze uxMaze;
+        private System.Windows.Forms.Button uxButton;
+    }
+}
+
---
+++ Ksu.Cis300.MazeSolver/MazeSolver.cs
@@ -0,0 +1,124 @@
+﻿/* Ksu.Cis300.MazeSolver.cs
+ * Author: Daniel J Lovell 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ksu.Cis300.MazeLibrary;
+
+namespace Ksu.Cis300.MazeSolver
+{
+    /// <summary>
+    /// The maze solver class generates a random maze, and solves it 
+    /// starting at a selected location. 
+    /// </summary>
+    public partial class MazeSolver : Form
+    {
+        /// <summary>
+        /// This initializes the maze solver program.
+        /// </summary>
+        public MazeSolver()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This button generates a new maze when the user clicks it.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxButton_Click(object sender, EventArgs e)
+        {
+            uxMaze.Generate();
+        }
+
+        /// <summary>
+        /// This method allows the user to enter, by mouse, his/her desired
+        /// starting location in the maze. The proram then calls a draw method 
+        /// to "solve the maze."
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxMaze_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            Cell startingLocation = uxMaze.GetCellFromPixel(e.Location);
+            if (uxMaze.IsInMaze(startingLocation))
+            {
+                
+                uxMaze.EraseAllPaths();
+                DrawPath(startingLocation);
+                
+                uxMaze.Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// The DrawPath method, when called, takes in the selected starting position 
+        /// and space by space finds the way out of the maze. As it progresses, spots
+        /// that are visited are stored in memory, and a Stack structure remembers 
+        /// the turn by turn directions the program follows, allowing it to "backtrack."
+        /// Once it is detected that it is outside of the maze, the program displays a bright
+        /// blue line tracing the path. If no solution can be found (closed box), the program
+        /// will display a message box stating the fact.
+        /// </summary>
+        /// <param name="c">This paremeter gives the program a starting location.</param>
+        public void DrawPath(Cell c)
+        {
+            
+            Stack<Direction> stack = new Stack<Direction>();
+            bool[,] pathHistory = new bool[uxMaze.MazeHeight, uxMaze.MazeWidth];
+            pathHistory[c.Row,c.Column] = true;
+            Direction d =  Direction.North;
+
+            while (uxMaze.IsInMaze(c)) 
+            {
+                if (d == Direction.North || d == Direction.East ||
+                    d == Direction.South || d == Direction.West)
+                {
+
+                    if (uxMaze.IsClear(c, d) && ((uxMaze.IsInMaze( uxMaze.Step(c, d) ) == false ||
+                       pathHistory[uxMaze.Step(c, d).Row, uxMaze.Step(c, d).Column] == false))  
+                       )
+                    {
+                        uxMaze.DrawPath(c, d);
+                        
+                        c = uxMaze.Step(c, d);
+                        stack.Push(d);
+                        d = Direction.North;
+
+                        if (uxMaze.IsInMaze(c))
+                        {
+                            pathHistory[c.Row, c.Column] = true;
+                        }
+                    }
+                    else 
+                    {
+                        d++;
+                    }
+                }
+
+                else if (d != Direction.North && d != Direction.East
+                    && d != Direction.South && d != Direction.West && stack.Count != 0)
+                {
+
+                    d = stack.Pop();
+                    c = uxMaze.ReverseStep(c, d);
+                    uxMaze.ErasePath(c, d);
+                    d++;
+                }
+                else 
+                {
+                    MessageBox.Show("There is no available exit path.");
+                    return;
+                }
+            } 
+        }
+    }
+}
